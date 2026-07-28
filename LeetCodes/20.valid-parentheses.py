1e726fb34fbddbcafdb68b1590667234726dc52f
백인기 <baekinki2026@gmail.com>
2026-07-29 00:16:47 +0900

    ste

--- LeetCodes/20.valid-parentheses.py
+++ LeetCodes/20.valid-parentheses.py
@@ -14,7 +14,25 @@ class Solution:
     
     def isValid(self, s: str) -> bool:
         stack = []
+        idx = 0
+        while idx < len(s):
+            target_char = s[idx]
+            
+            if target_char in self.str_dict:
+                stack.append(target_char)  
+            else:
+                if not stack:
+                    return False
+                
+                pop_char = stack.pop()
+                if self.str_dict[pop_char] != target_char:
+                    return False
+            idx += 1
         
+        if stack:
+            return False
+        else:
+            return True
         
         
         

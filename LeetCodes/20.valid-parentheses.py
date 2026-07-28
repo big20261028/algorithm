76a71ee4d0393b0323ee8f505b62d7140e413b7e
백인기 <baekinki2026@gmail.com>
2026-07-28 23:37:16 +0900

    set

---
+++ LeetCodes/20.valid-parentheses.py
@@ -0,0 +1,22 @@
+#
+# @lc app=leetcode id=20 lang=python3
+#
+# [20] Valid Parentheses
+#
+
+# @lc code=start
+class Solution:
+    str_dict = {
+        "(" : ")",
+        "{" : "}",
+        "[" : "]"
+    }
+    
+    def isValid(self, s: str) -> bool:
+        stack = []
+        
+        
+        
+        
+# @lc code=end
+

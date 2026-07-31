cf7513068226f0ba6543139e2668658d236e064a
big20261028 <baekinki2026@gmail.com>
2026-07-31 23:47:33 +0900

    feat: reetcode.removeDuplicates Clear

--- LeetCodes/Python/26.remove-duplicates-from-sorted-array.py
+++ LeetCodes/Python/26.remove-duplicates-from-sorted-array.py
@@ -7,6 +7,15 @@
 # @lc code=start
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        
+        target_idx = 1
+        target_num = nums[0]
+        for i in range(1, len(nums)):
+            if target_num < nums[i]:
+                nums[target_idx] = nums[i]
+                target_idx += 1
+                target_num = nums[i]
+
+        return target_idx
+
 # @lc code=end
 

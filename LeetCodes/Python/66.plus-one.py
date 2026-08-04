58d4729b0e8fc9a3442926f7834fcf17569cf070
big20261028 <baekinki2026@gmail.com>
2026-08-04 23:45:22 +0900

    backup

---
+++ LeetCodes/Python/66.plus-one.py
@@ -0,0 +1,22 @@
+#
+# @lc app=leetcode id=66 lang=python3
+#
+# [66] Plus One
+#
+
+# @lc code=start
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        target_idx = len(digits) - 1 
+
+        while target_idx >=
+
+
+
+        
+        
+            
+        
+        
+# @lc code=end
+

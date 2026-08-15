2c549486f096e9ef98d0424be1eb8fd983335f45
big20261028 <baekinki2026@gmail.com>
2026-08-15 23:21:55 +0900

    leetcode 69 solved

--- LeetCodes/Python/69.sqrt-x.py
+++ LeetCodes/Python/69.sqrt-x.py
@@ -5,10 +5,31 @@
 #
 
 # @lc code=start
+import math
+
+
 class Solution:
     def mySqrt(self, x: int) -> int:
-        print("test")
-        print('second test')
+        if x < 2:
+            return x
+
+        left, right = 1, x // 2
+        result = 0
+
+        while left <= right:
+            mid = (left + right) // 2
+            square = mid * mid
+
+            if square == x:
+                return mid
+            elif square < x:
+                result = mid 
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        return result
+        
         
 # @lc code=end
 

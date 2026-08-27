121dc2906c34c370ab26f8e2a757a63d7676b461
big20261028 <baekinki2026@gmail.com>
2026-08-27 23:04:05 +0900

    leetcode 108 solved

--- LeetCodes/Python/108.convert-sorted-array-to-binary-search-tree.py
+++ LeetCodes/Python/108.convert-sorted-array-to-binary-search-tree.py
@@ -13,7 +13,42 @@
 #         self.right = right
 class Solution:
     def sortedArrayToBST(self, nums: List[int]) -> Optional[TreeNode]:
-        print("test")
+        # val_list = list()
+        # self.makeList(nums, val_list)
+        new_tree = TreeNode()
+        self.makeNewTree(new_tree, nums)
+        return new_tree
+
+    # def makeList(self, node, temp_list):
+    #     if not node:
+    #         return
+
+    #     temp_list.append(node.val)
+
+    #     if not node.left:
+    #         self.makeList(node.left, temp_list)
+
+    #     if not node.right:
+    #         self.makeList(node.right, temp_list)
+
+    def makeNewTree(self, node, temp_list):
+        if not temp_list:
+            node.val = None
+            return
+
+        center_idx = len(temp_list) // 2
+
+        node.val = temp_list[center_idx]
+
+        if temp_list[:center_idx]:
+            node.left = TreeNode()
+            self.makeNewTree(node.left, temp_list[:center_idx])
+
+        if temp_list[center_idx + 1:]:
+            node.right = TreeNode()
+            self.makeNewTree(node.right, temp_list[center_idx + 1:])
+        # node.right = temp_list[center_idx + 1:]
+
         
 # @lc code=end
 

a40430a1f242647c1346313e68db8d6aec63ed6c
big20261028 <baekinki2026@gmail.com>
2026-09-04 23:46:23 +0900

    test

---
+++ LeetCodes/Python/125.valid-palindrome.py
@@ -0,0 +1,17 @@
+#
+# @lc app=leetcode id=125 lang=python3
+#
+# [125] Valid Palindrome
+#
+
+# @lc code=start
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        # s.isdecimal()
+        # s.isalpha()
+        # print(s.lower())
+        s = s.lower()
+
+        
+# @lc code=end
+

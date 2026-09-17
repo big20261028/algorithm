975b5129ad9358b22d922e6ba8dd4ce26d470a92
big20261028 <baekinki2026@gmail.com>
2026-09-17 22:22:50 +0900

    leetcode 168 solved

--- LeetCodes/Python/168.excel-sheet-column-title.py
+++ LeetCodes/Python/168.excel-sheet-column-title.py
@@ -7,6 +7,25 @@
 # @lc code=start
 class Solution:
     def convertToTitle(self, columnNumber: int) -> str:
+        result_list = list()
+        
+        while columnNumber:
+            columnNumber -= 1
+            left_val = columnNumber % 26
+            # if not left_val:
+            #     left_val = 26
+            # left_val -= 1
+            target_char = chr(65 + left_val)
+            result_list.append(target_char)
+
+
+            columnNumber //= 26
+        result_list.reverse()
+        return "".join(result_list)
+
+
+
+
         
 # @lc code=end
 
--- LeetCodes/Python/test.py
+++ LeetCodes/Python/test.py
@@ -1,1 +1,14 @@
-print(list(5))
+test_char = 'A'
+print(ord(test_char))
+test_char2 = 'Z'
+print(ord(test_char2))
+
+
+
+# temp_list = list()
+
+# temp_list[0] = 'A'
+# temp_list[1] = 'A'
+
+# print(temp_list)
+
